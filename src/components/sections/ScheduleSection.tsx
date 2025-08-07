0f59d80f2ff9213df81e7b094ffbe80b681e2fd1
riidlHack <riidlhack@gmail.com>
2025-08-07 07:24:00 +0530

    updated schedule hovering issue

--- src/components/sections/ScheduleSection.tsx
+++ src/components/sections/ScheduleSection.tsx
@@ -80,7 +80,7 @@ const TimelineItem = ({ date, title, description, isLeft }) => {
         <div className={`w-1/2 ${isLeft ? 'pr-8' : ''}`}>
           {isLeft && (
             // <div className="bg-gradient-to-r from-green-500 to-blue-500 p-6 rounded-lg shadow-lg border border-gray-200 w-full ml-4">
-            <div className='p-6 rounded-3xl shadow-lg border border-neon-green/30 w-full ml-4 transition-transform duration-300 ease-in-out hover:scale-80 cursor-pointer bg-[#0a0a0a] py-12 glow-desktop-hover'>
+            <div className='p-6 rounded-3xl shadow-lg border border-neon-green/30 w-full ml-4 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer bg-[#0a0a0a] py-12 glow-desktop-hover'>
               <p className="text-sm text-[#00ffb7] mb-2 font-bold">{date}</p>
               <h3 className="text-xl font-semibold text-white mb-2">{title}</h3>
               <p className="text-gray-700 text-sm">{description}</p>
@@ -94,7 +94,7 @@ const TimelineItem = ({ date, title, description, isLeft }) => {
         {/* Right content block */}
         <div className={`w-1/2 ${!isLeft ? 'pl-8' : ''}`}>
           {!isLeft && (
-            <div className='p-6 rounded-3xl shadow-lg border border-neon-green/30 w-full ml-4 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer relative bg-[#0a0a0a] py-12 glow-desktop-hover'>
+            <div className='p-6 rounded-3xl shadow-lg border border-neon-green/30 w-full ml-6 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer relative bg-[#0a0a0a] py-12 glow-desktop-hover'>
               <p className="text-sm text-[#00ffb7] mb-2 font-bold">{date}</p>
               <h3 className="text-xl font-semibold text-white mb-2">{title}</h3>
               <p className="text-gray-700">{description}</p>
